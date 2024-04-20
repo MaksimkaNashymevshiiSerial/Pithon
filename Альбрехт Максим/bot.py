dba166514796c437ad07685bb5faca90de2a9130
MaksimkaNashymevshiiSerial <161151879+MaksimkaNashymevshiiSerial@users.noreply.github.com>
2024-04-20 20:00:11 +0300

    Add files via upload

--- Альбрехт Максим/bot.py
+++ Альбрехт Максим/bot.py
@@ -4,6 +4,13 @@ import random
 bot = telebot.TeleBot('7180757450:AAHKF_9iws9kVMj4xGodc1AAXfcKYHu_YW0')
 flag = False
 secret = 0
+count = 0
+
+def random_symbol(st, count):
+	res = ''
+	for i in range(count):
+		res = res + random.choice(list(st))
+	return res
 
 @bot.message_handler(content_types=
 	['text', 'document', 'audio'])
@@ -11,6 +18,7 @@ secret = 0
 def get_text_messages(message):
 	global flag
 	global secret
+	global count
 
 	print(message)
 	body = message.text
@@ -19,22 +27,43 @@ def get_text_messages(message):
 			"Привет!, поиграем? </br> Напиши /help для получения правил!")
 	elif body =="/help":
 		bot.send_message(message.from_user.id,
-			"Для запуска игры напиши /secret_number")
+			"Для запуска игры напиши /secret_number"
+			"\n Для запуска генератора паролей /passwd")
 	elif body =="/secret_number":
 		flag = "secret_number"
+		count = 0
 		secret = random.randrange(0, 100)
 		bot.send_message(message.from_user.id,
 			"Вы выбрали игру: Угадай число! Я загадал число от 0 до 100 ... попробуй угадать!")
+	elif body =="/passwd":
+		lowercase = 'qwertyuiopasdfghjklzxcvbnm'
+		digit = '1234567890'
+		uppercase = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+		lowercase_count = 4
+		digit_count = 2
+		uppercase_count = 3
+		punctuation = '!@#$%^&*()_+"№;%:?*='
+		punctuation_count = 2
+		passwd = random_symbol(lowercase, lowercase_count)
+		passwd = passwd + random_symbol(digit, digit_count)
+		passwd = passwd + random_symbol(uppercase, uppercase_count)
+		passwd = passwd + random_symbol(punctuation, punctuation_count)
+		l = list(passwd)
+		random.shuffle(l)
+		passwd = ''.join(l)
+		bot.send_message(message.from_user.id, passwd)
+
 	elif flag == "secret_number":
+		count += 1
 		if  int(body) > secret:
 			bot.send_message(message.from_user.id,
-				"Ваше число меньше")
+				"Ваше число меньше, Попыток: " + str(count))
 		elif int(body) < secret:
 			bot.send_message(message.from_user.id,
-				"Ваше число больше")
+				"Ваше число больше, Попыток: " + str(count))
 		else:
 			bot.send_message(message.from_user.id,
-				"Это победа, ты молодец, Пользователь)")
+				"Это победа, ты молодец, Пользователь), Попыток: " + str(count))
 
 	else:
 		bot.send_message(message.from_user.id,
